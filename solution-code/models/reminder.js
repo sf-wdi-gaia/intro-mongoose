431bcf018fb7658f07e4a6f548b38331394311b9
Alex White <awhit012@gmail.com>
2016-05-24 09:05:58 -0700

    updates solution

--- solution-code/models/reminder.js
+++ solution-code/models/reminder.js
@@ -1,13 +1,11 @@
-// requiring mongoose dependency
 var mongoose = require('mongoose');
 
-// defining schema for reminders
 var ReminderSchema = new mongoose.Schema({
   title: String,
   body: String,
-  createdAt: { type : Date, default: Date.now() }
+  createdAt: {type : Date, default: new Date()}
 });
-// define the model
+
 var Reminder = mongoose.model("Reminder", ReminderSchema);
-// export the model to any files that `require` this one
+
 module.exports = Reminder;
